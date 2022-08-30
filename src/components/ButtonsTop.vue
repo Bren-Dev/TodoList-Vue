e26c7746eaabacda1bca32ce03136eafe7fc5ca1
Bren-Dev <brenda_nogueira_pe@hotmail.com>
2022-08-30 07:05:36 -0300

    ajuste

--- src/components/ButtonsTop.vue
+++ src/components/ButtonsTop.vue
@@ -31,46 +31,6 @@ input{
     margin: 20px 0 10px 0;
 }
 
-.titleTask{
-  font-size: 1rem;
-  font-weight: 500;
-  color: #000;
-}
-
-.divIcons{
-     display: flex;
-}
-
-.iconExclamation{
-  border: green 1px solid;
-  padding: 5px;
-  margin-right: 10px;
-  cursor: pointer;
-  border-radius: 2px;
-      color: green;
-}
-
-.iconTrash{
-
-   border: red 1px solid;
-  padding: 5px;
-  cursor: pointer;
-  border-radius: 2px;
-  color: red;
-}
-
-.wrapperElementsTodo{
-    width: 100%;
-    background-color: white;
-    border-radius: 3px;
-    border: 1px solid #D1D2D6;
-    margin-top: 20px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 16px 20px;
-}
-
 .header{
   display: flex;
   justify-content: space-between;
